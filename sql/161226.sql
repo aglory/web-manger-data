2a6309e0e67e267aef3df8caec74f785016894c1
aglory <Fireman_duck@126.com>
2016-12-26 16:14:37 +0800

    sql补全

--- sql/161226.sql
+++ sql/161226.sql
@@ -3,3 +3,5 @@ alter table tbUserInfo
 modify Id int primary key auto_increment,
 add AccountId int not null default 0 comment '登录账号编号' after `Id`,
 add `DateTimeCreate` datetime DEFAULT NULL COMMENT '修改时间' after Img;
+
+update tbUserInfo set AccountId = Id;
